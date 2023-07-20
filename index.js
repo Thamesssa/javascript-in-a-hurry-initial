06bb0cb499edf7ce4c0fc5d07a21c5bf4185641f
thamesssa <thamesssa@outlook.com>
2023-07-20 15:16:50 +0200

    temprature conversion funtionality aupdated

--- index.js
+++ index.js
@@ -18,14 +18,35 @@ const userLocation = "London";
 let temperature = 22.8673;
 
 //create a weather text variable
-let weatherText = `The weather is ${weatherCondition } and ${temperature.toFixed(1)} degrees in ${userLocation}.`;
+let celsiusText = `The weather is ${weatherCondition } and ${temperature.toFixed(1)} °C n ${userLocation}.`;
+//farh text
+let farhText = `The weather is ${weatherCondition } and ${convertCelsiusToFahrenheit(temperature.toFixed(1))} °F in ${userLocation}.`;
+
 
 
 
 
 document.querySelector("#greeting").innerHTML = greetingText;
-document.querySelector('p#weather').innerHTML = weatherText;
+document.querySelector('p#weather').innerHTML = celsiusText;
 
+
+//a function to convert celsius to fahrenheit
+function convertCelsiusToFahrenheit(temperature) {
+    const fahr = temperature * 1.8 + 32;
+    return   fahr;
+}
+
+//add an event listener to the to weather-group
+document.querySelector(".weather-group").addEventListener("click", function(event){
+  if(event.target.id=="celsius") {
+   document.querySelector('p#weather').innerHTML = celsiusText;
+  }else if(event.target.id=="fahr"){
+   document.querySelector('p#weather').innerHTML = farhText;
+  }
+
+
+     
+});
 
 
 
