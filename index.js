4caeccdadb16504fd87c038dff5882fea33f5506
thamesssa <thamesssa@outlook.com>
2023-07-21 19:37:31 +0200

    local time section updated

--- index.js
+++ index.js
@@ -51,6 +51,14 @@ document.querySelector(".weather-group").addEventListener("click", function(even
 
 
 
+setInterval(function(){
+  let localTime = new Date();
+
+  document.querySelector("span[data-time =hours]").textContent = localTime.getHours().toString().padStart(2,"0");
+  document.querySelector("span[data-time =minutes]").textContent = localTime.getMinutes().toString().padStart(2,"0");
+  document.querySelector("span[data-time =seconds]").textContent = localTime.getSeconds().toString().padStart(2,"0");
+  
+},1000)
 
 
 
